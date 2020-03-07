bd00eb625e1d971e98a3f0faf3401f5bd90a04cc
fangjinuo <fs1194361820@163.com>
2020-03-07 09:13:15 +0800

    * office test case

---
+++ src/main/java/com/jn/camunda/demo/WebappExampleProcessApplication.java
@@ -0,0 +1,11 @@
+package com.jn.camunda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappExampleProcessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebappExampleProcessApplication.class, args);
+    }
+}
